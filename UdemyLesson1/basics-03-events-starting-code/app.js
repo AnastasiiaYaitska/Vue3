1744e7e6c3b90cfcb80412c5b373802ce60045d8
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2024-03-05 14:46:08 +0100

    added watcher and computed p.

--- UdemyLesson1/basics-03-events-starting-code/app.js
+++ UdemyLesson1/basics-03-events-starting-code/app.js
@@ -3,8 +3,41 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      lastName: "",
       confirmName: "",
+      //fullName: "",
     };
+  },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(() => {
+          that.counter = 0;
+        }, 2000);
+      }
+    },
+    // name(newValue, oldValue) {
+    //   if (newValue === "") {
+    //     return "";
+    //   }
+    //   return (this.fullName = newValue + " " + this.fullName);
+    // },
+    // lastName(newValue, oldValue) {
+    //   if (newValue === "") {
+    //     return "";
+    //   }
+    //   return (this.fullName = this.name + " " + newValue);
+    // },
+  },
+  computed: {
+    fullName() {
+      console.log("Running again");
+      if (this.name === "" || this.lastName === "") {
+        return "";
+      }
+      return this.name + " " + this.lastName;
+    },
   },
   methods: {
     add() {
@@ -15,15 +48,25 @@ const app = Vue.createApp({
       this.counter--;
     },
 
-    setName(event, secondName) {
-      this.name = event.target.value + " " + secondName;
-    },
+    // outputFullName() {
+    //   console.log("Running again");
+    //   if (this.name === "") {
+    //     return "";
+    //   }
+    //   return this.name + " " + "Brian";
+    // },
 
+    // setName(event, secondName) {
+    //   this.name = event.target.value + " " + secondName;
+    // },
+    resetName() {
+      this.name = "";
+    },
     submit() {},
 
-    confirmInput() {
-      this.confirmName = this.name;
-    },
+    // confirmInput() {
+    //   this.confirmName = this.name;
+    // },
   },
 });
 
