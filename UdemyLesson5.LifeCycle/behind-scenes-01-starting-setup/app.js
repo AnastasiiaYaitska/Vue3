d54c87fc89e0bec01d9978e7ad159e336fba4f28
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2024-03-07 10:58:10 +0100

    instance cycle

---
+++ UdemyLesson5.LifeCycle/behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,47 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: "",
+      message: "Vue is great!",
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.$refs.textMessage.value;
+      console.log(this.$refs.textMessage); //exit to dom element without controlling every key typing in input
+    },
+  },
+  beforeCreate() {
+    console.log("beforeCreate()");
+  },
+  created() {
+    console.log("create()");
+  },
+  beforeMount() {
+    console.log("beforeMount()");
+  },
+  mounted() {
+    console.log("mounted()");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate()");
+  },
+  updated() {
+    console.log("updated()");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount()");
+  },
+  unmounted() {
+    console.log("unmounted()");
+  },
+});
+
+app.mount("#app");
+
+// setTimeout(() => {
+//   app.unmount();
+// }, 3000);
