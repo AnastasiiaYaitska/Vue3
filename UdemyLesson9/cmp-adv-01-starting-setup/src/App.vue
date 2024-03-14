9860acc54ff708b8c45b1a262afede520c9cd824
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2024-03-14 08:45:39 +0100

    global/local components

--- UdemyLesson9/cmp-adv-01-starting-setup/src/App.vue
+++ UdemyLesson9/cmp-adv-01-starting-setup/src/App.vue
@@ -1,23 +1,34 @@
 <template>
   <div>
-    <the-header></the-header>
+    <!-- <the-header></the-header> -->
+    <TheHeader />
     <badge-list></badge-list>
-    <user-info
+    <UserInfo
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
-    ></user-info>
+    />
   </div>
 </template>
 
 <script>
+import TheHeader from "./components/TheHeader.vue";
+import UserInfo from "./components/UserInfo.vue";
+import BadgeList from "./components/BadgeList.vue";
 export default {
+  //local components registration. Available locale
+  components: {
+    // "the-header": TheHeader,    if you registered components like this , they are available like open-close cebab-case components
+    BadgeList, //if vi registered components like this , they are available in both sintakse
+    TheHeader,
+    UserInfo,
+  },
   data() {
     return {
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
       },
     };
   },
