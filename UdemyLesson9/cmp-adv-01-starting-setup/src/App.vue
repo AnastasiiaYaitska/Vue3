590c71b0ae1354476acb457ad0520a6233ec3c82
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2024-03-14 12:22:51 +0100

    teleport and custom alerts

--- UdemyLesson9/cmp-adv-01-starting-setup/src/App.vue
+++ UdemyLesson9/cmp-adv-01-starting-setup/src/App.vue
@@ -2,44 +2,61 @@
   <div>
     <!-- <the-header></the-header> -->
     <TheHeader />
-    <badge-list></badge-list>
+    <button @click="setSelectedComponent('active-goals')">Active goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage goals</button>
+    <!-- allows to keep component and not destroy it after switch, work only with <component>  ( dynamic component) -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
+    <!-- <badge-list></badge-list>
     <UserInfo
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
     />
-    <CourseGoals #default="slotProps">
-      <!-- <template #default="slotProps"> -->
-      <h2>{{ slotProps.goal }}</h2>
-      <p>{{ slotProps["another-prop"] }}</p>
-      <!-- </template> -->
-    </CourseGoals>
+    <CourseGoals #default="slotProps"> -->
+    <!-- <template #default="slotProps"> -->
+    <!-- <h2>{{ slotProps.goal }}</h2>
+      <p>{{ slotProps["another-prop"] }}</p> -->
+    <!-- </template> -->
+    <!-- </CourseGoals> -->
   </div>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
-import UserInfo from "./components/UserInfo.vue";
-import BadgeList from "./components/BadgeList.vue";
-import CourseGoals from "./components/CourseGoals.vue";
+import ActiveGoals from "./components/ActiveGoals";
+import ManageGoals from "./components/ManageGoals.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import CourseGoals from "./components/CourseGoals.vue";
 
 export default {
   //local components registration. Available locale
   components: {
     // "the-header": TheHeader,    if you registered components like this , they are available like open-close cebab-case components
-    BadgeList, //if vi registered components like this , they are available in both sintakse
     TheHeader,
-    UserInfo,
-    CourseGoals,
+    ActiveGoals,
+    ManageGoals,
+    //   BadgeList, //if vi registered components like this , they are available in both sintakse
+    //   UserInfo,
+    //   CourseGoals,
   },
   data() {
     return {
-      activeUser: {
-        name: "Maximilian Schwarzmüller",
-        description: "Site owner and admin",
-        role: "admin",
-      },
+      selectedComponent: "active-goals",
+      // activeUser: {
+      //   name: "Maximilian Schwarzmüller",
+      //   description: "Site owner and admin",
+      //   role: "admin",
+      // },
     };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+      console.log(cmp);
+    },
   },
 };
 </script>
---
+++ UdemyLesson9/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2>Manage goals</h2>
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Save my goal</button>
+    <teleport to="body">
+      <error-alert v-if="inputIsInvalid">
+        <h2>Input is invalid</h2>
+        <p>Insert at least 2 characters....</p>
+        <button @click="confirmAlert">Ok</button>
+      </error-alert>
+    </teleport>
+  </div>
+</template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+export default {
+  components: { ErrorAlert },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === "") {
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmAlert() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
+</script>
