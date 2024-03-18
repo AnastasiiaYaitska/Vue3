acc1100dcc4c0f52b8f88a590a3b38ef832572cc
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2024-03-18 21:47:21 +0100

    handling incorrect url

---
+++ UdemyLesson13.routing/routing-01-starting-setup/src/components/nav/NotFound.vue
@@ -0,0 +1,5 @@
+<template>
+<p>This page is not found</p>
+<router-link to="/teams"> Go to teams page</router-link>
+
+</template>
--- UdemyLesson13.routing/routing-01-starting-setup/src/main.js
+++ UdemyLesson13.routing/routing-01-starting-setup/src/main.js
@@ -5,13 +5,18 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    { path: '/', redirect: '/teams' }, //in this case we redirect our user to another url and change url in search line
+    // { path: '/teams', component: TeamsList , alias: '/},      in this case we rerender our component on path '/ ' and url is doesn't change.
+
     { path: '/teams', component: TeamsList },
     { path: '/users', component: UsersList },
     { path: '/teams/:teamId', component: TeamMembers, props: true },
+    { path: '/:notFound(.*)', component: NotFound },
   ],
   linkActiveClass: 'active', //adding custom name to active class, instead of default 'router-link-active'
 });
