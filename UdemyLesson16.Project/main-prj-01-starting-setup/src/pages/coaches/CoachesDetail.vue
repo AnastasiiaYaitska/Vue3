9791b3569f3ef8b912c18893324437a01faaa2c6
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2024-03-28 20:32:58 +0100

    base settings and routing

---
+++ UdemyLesson16.Project/main-prj-01-starting-setup/src/pages/coaches/CoachesDetail.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>Details</div>
+  <router-view></router-view>
+  <router-link to="/coaches/c1/contact">To contact</router-link>
+</template>
---
+++ UdemyLesson16.Project/main-prj-01-starting-setup/src/router.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import CoachesDetail from './pages/coaches/CoachesDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachesRegistration from './pages/coaches/CoachesRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestReceived from './pages/requests/RequestReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: CoachesList },
+    {
+      path: '/coaches/:id',
+      component: CoachesDetail,
+      children: [{ path: 'contact', component: ContactCoach }], /// /coaches/76r7364/contact
+    },
+    { path: '/register', component: CoachesRegistration },
+    { path: '/request', component: RequestReceived },
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
+});
+
+export default router;
