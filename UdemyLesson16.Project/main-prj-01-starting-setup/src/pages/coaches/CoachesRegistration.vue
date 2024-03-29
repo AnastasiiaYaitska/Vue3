58b12d456d92bddda9c54d148fd56da275e061b1
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2024-03-29 21:07:19 +0100

    added registration form

--- UdemyLesson16.Project/main-prj-01-starting-setup/src/pages/coaches/CoachesRegistration.vue
+++ UdemyLesson16.Project/main-prj-01-starting-setup/src/pages/coaches/CoachesRegistration.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>Registration</div>
+  <section>
+    <base-card>
+      <h3>Registration as a coach!</h3>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+  components: {
+    CoachForm,
+  },
+};
+</script>
